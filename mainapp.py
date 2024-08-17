4f6c3badad3acce1cdfdb444ee4c1ea4227455e4
Hugo Rios Brito <53199384+TecHugoRios@users.noreply.github.com>
2024-08-17 15:42:16 -0300

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -1,119 +1,171 @@
-from tkinter import *
-import tkinter as tk
+import dearpygui.dearpygui as dpg
 import pandas as pd
+import os
 
-menu_principal = tk.Tk()
-menu_principal.title("Minhas Vendas")
-menu_principal['bg'] = "#f0f8ff"  # Mudança de cor de fundo
-menu_principal.iconbitmap("imgs/icon.ico")
+dpg.create_context()
 
-# Dimensões da janela
-largura = 1200
-altura = 800
+class xlsx():
+    def xlsx_state():
+         # Função para verificar se um arquivo .xlsx existe na pasta do arquivo principal
+        diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+        nome_arquivo = "dados.xlsx"
+        pasta_arquivo = os.path.join(diretorio_atual, "dados")
+        os.makedirs(pasta_arquivo, exist_ok=True)
+        path = os.path.join(pasta_arquivo, nome_arquivo)
+        exists = os.path.exists(path)
 
-# Resolução do sistema
-largura_screen = menu_principal.winfo_screenwidth()
-altura_screen = menu_principal.winfo_screenheight()
+        if not exists:
+            dados = {
+                'Nome Completo': [],
+                'Produto': [],
+                'Seguradora': [],
+                'Valor': [],
+                '(%) Comissão': [],
+                'Valor de Comissão': [],
+                'Valor do Crédito': [],
+                'Data do Crédito': [],
+                'Vigência Final': [],
+                'Indicado': [],
+                'Indicador': [],
+                'Valor de Comissão do Indicador': []
+            }
 
-# Posição da janela
-posx = largura_screen / 2 - largura / 2
-posy = altura_screen / 2 - altura / 2
+            df = pd.DataFrame(dados)
 
-menu_principal.resizable(0, 0)  # Não permitir redimensionamento
+            try:
+                # Cria o arquivo Excel se ele não existir
+                df.to_excel(path, sheet_name='dados', index=False)
+            except Exception as e:
+                print(f"Erro ao criar o arquivo: {e}")
 
-# GUI
-frame = Frame(menu_principal, padx=20, pady=20, bg="#e0f7fa")  # Fundo diferente para o frame
-frame.grid(column=0, row=0, padx=20, pady=20, sticky="nsew")
+        return path
 
-# Configurar redimensionamento das colunas e linhas
-menu_principal.grid_rowconfigure(0, weight=1)
-menu_principal.grid_columnconfigure(0, weight=1)
-frame.grid_rowconfigure(0, weight=1)
-frame.grid_columnconfigure(0, weight=1)
+class XlsxPath(xlsx):
+    def get_path(self):
+        # Retorna o caminho do arquivo .xlsx usando a função herdada
+        return self.xlsx_state()
 
-Label(frame, text="Bem-vindo, cadastre sua venda:", font="Poppins 15 bold", bg="#e0f7fa").grid(column=0, row=0,
-                                                                                               columnspan=2,
-                                                                                               pady=10)
-# Estilo dos Labels
+def text_inputs():
 
-label_nome = Label(frame, text="Nome Completo:", bg="#e0f7fa", font="Poppins 10 bold")
-label_nome.grid(column=0, row=1, sticky=E, padx=5, pady=5)
+    dpg.add_input_text(label="Nome Completo", tag="input_txtName")
+    dpg.add_input_text(label="Produto", tag="input_txtProduct")
+    dpg.add_input_text(label="Seguradora", tag="input_txtInsCompany")
+    dpg.add_input_text(label="Valor", tag="input_txtInsValue")
+    dpg.add_input_text(label="(%) Comissão", tag="input_txtCommissionPct")
+    dpg.add_input_text(label="Valor de Comissão", tag="input_txtCommissionValue")
+    dpg.add_input_text(label="Valor do Crédito", tag="input_txtCreditValue")
+    dpg.add_input_text(label="Data do Crédito", tag="input_txtCreditDate")
+    dpg.add_input_text(label="Vigência Final", tag="input_txtEndDate")
+    dpg.add_checkbox(label="Indicado?", tag="input_txtIndicated")
+    dpg.add_input_text(label="Indicador", tag="input_txtIndicator")
+    dpg.add_input_text(label="Valor de Comissão do Indicador", tag="input_txtIndicatorCommissionValue")
 
-label_produto = Label(frame, text="Produto:", bg="#e0f7fa", font="Poppins 10 bold")
-label_produto.grid(column=0, row=2, sticky=E, padx=5, pady=5)
+def button_callback():
+    name = dpg.get_value("input_txtName")
+    product = dpg.get_value("input_txtProduct")
+    InsCompany = dpg.get_value("input_txtInsCompany")
+    InsValue = dpg.get_value("input_txtInsValue")
+    commission_pct = dpg.get_value("input_txtCommissionPct")
+    commission_value = dpg.get_value("input_txtCommissionValue")
+    credit_value = dpg.get_value("input_txtCreditValue")
+    credit_date = dpg.get_value("input_txtCreditDate") # mudar para formato data
+    end_date = dpg.get_value("input_txtEndDate") # mudar para formato data
+    indicated = dpg.get_value("input_txtIndicated") # mudar para radio button 
+    indicator = dpg.get_value("input_txtIndicator")
+    indicator_commission_value = dpg.get_value("input_txtIndicatorCommissionValue")
+    
+    fields = {
+        "Nome Completo": name,
+        "Produto": product,
+        "Seguradora": InsCompany,
+        "Valor": InsValue,
+        "(%) Comissão": commission_pct,
+        "Valor de Comissão": commission_value,
+        "Valor do Crédito": credit_value,
+        "Data do Crédito": credit_date,
+        "Vigência Final": end_date
+    }
 
-label_seguradora = Label(frame, text="Seguradora:", bg="#e0f7fa", font="Poppins 10 bold")
-label_seguradora.grid(column=0, row=3, sticky=E, padx=5, pady=5)
+    empty_fields = [fields for fields, value in fields.items() if not value or value.strip() == ""]
 
-label_valor = Label(frame, text="Valor:", bg="#e0f7fa", font="Poppins 10 bold")
-label_valor.grid(column=0, row=4, sticky=E, padx=5, pady=5)
+    if empty_fields:
+        empty_fields_str = ", \n".join(empty_fields)
+        with dpg.window(label="Aviso", pos=(200, 100), no_close=False, popup=True, horizontal_scrollbar=True,max_size=[300,200]):
+            dpg.add_text(f"Os seguintes campos estão vazios: \n{empty_fields_str}")
+        return
+    if indicated ==  False:
+        pass
 
-label_comissao = Label(frame, text="Comissão (%):", bg="#e0f7fa", font="Poppins 10 bold")
-label_comissao.grid(column=0, row=5, sticky=E, padx=5, pady=5)
+    data_dictionary = {  'Nome Completo': [name],
+                        'Produto': [product],
+                        'Seguradora': [InsCompany],
+                        'Valor': [InsValue],
+                        '(%) Comissão': [commission_pct],
+                        'Valor de Comissão': [commission_value],
+                        'Valor do Crédito': [credit_value],
+                        'Data do Crédito': [credit_date],
+                        'Vigência Final': [end_date],
+                        'Indicado': [indicated],
+                        'Indicador': [indicator],
+                        'Valor de Comissão do Indicador': [indicator_commission_value]
+                    }
+    
+    tabel = pd.DataFrame.from_dict(data_dictionary)
+    if os.path.exists('dados/dados.xlsx'):
+        existing_df = pd.read_excel('dados/dados.xlsx')
 
-label_valorComissao = Label(frame, text="Valor da Comissão:", bg="#e0f7fa", font="Poppins 10 bold")
-label_valorComissao.grid(column=0, row=6, sticky=E, padx=5, pady=5)
+        if any(existing_df['Nome Completo'] == name):
+            with dpg.window(label="Aviso", pos=(200, 100), no_close=False, popup=True, horizontal_scrollbar=True,autosize=True, max_size=[300,50]):
+                dpg.add_text("Venda já cadastrada")
+            return  
+        else:
+            updated_df = pd.concat([existing_df, tabel], ignore_index=True)
+    else:
+        # Se o arquivo não existir, cria um novo DataFrame
+        updated_df = tabel
+    
+    # Salva o DataFrame atualizado no arquivo Excel
+    updated_df.to_excel('dados/dados.xlsx', index=False)
 
-label_valorCredito = Label(frame, text="Valor de Crédito:", bg="#e0f7fa", font="Poppins 10 bold")
-label_valorCredito.grid(column=0, row=7, sticky=E, padx=5, pady=5)
+    xlsx_path = XlsxPath.get_path(self=XlsxPath)
 
-label_dataCredito = Label(frame, text="Data de Crédito:", bg="#e0f7fa", font="Poppins 10 bold")
-label_dataCredito.grid(column=0, row=8, sticky=E, padx=5, pady=5)
+    with dpg.window(label="Mensagem", pos=(50, 200), no_close=False):
+        dpg.add_text(f'Cliente "{name}" cadastrado com sucesso!')
+        dpg.add_text(f"Dados salvos com sucesso em {xlsx_path}")
 
-label_VigenciaFinal = Label(frame, text="Vigência Final:", bg="#e0f7fa", font="Poppins 10 bold")
-label_VigenciaFinal.grid(column=0, row=9, sticky=E, padx=5, pady=5)
-
-label_Indicador = Label(frame, text="Indicador:", bg="#e0f7fa", font="Poppins 10 bold")
-label_Indicador.grid(column=0, row=10, sticky=E, padx=5, pady=5)
-
-label_valorComissaoIndicador = Label(frame, text="Valor da Comissão para o indicador:", bg="#e0f7fa",
-                                     font="Poppins 10 bold")
-label_valorComissaoIndicador.grid(column=0, row=11, sticky=E, padx=5, pady=5)
-
-# Mensagem de rodapé
-Label(frame, text="Desenvolvido por [Hugo Rios]", bg="#e0f7fa", font="Poppins 8 italic").grid(column=0, row=13,
-                                                                                              columnspan=2,
-                                                                                              pady=10)
-
-# Estilo da Entry
-NomeCompleto = Entry(frame, width=30, font="Poppins 10")
-NomeCompleto.grid(column=1, row=1, pady=5, sticky="ew", padx=5)
-
-Produto = Entry(frame, width=30, font="Poppins 10")
-Produto.grid(column=1, row=2, pady=5, sticky="ew", padx=5)
-
-Seguradora = Entry(frame, width=30, font="Poppins 10")
-Seguradora.grid(column=1, row=3, pady=5, sticky="ew", padx=5)
-
-Valor = Entry(frame, width=30, font="Poppins 10")
-Valor.grid(column=1, row=4, pady=5, sticky="ew", padx=5)
-
-Comissao = Entry(frame, width=30, font="Poppins 10")
-Comissao.grid(column=1, row=5, pady=5, sticky="ew", padx=5)
-
-ValorComissao = Entry(frame, width=30, font="Poppins 10")
-ValorComissao.grid(column=1, row=6, pady=5, sticky="ew", padx=5)
-
-ValorCredito = Entry(frame, width=30, font="Poppins 10")
-ValorCredito.grid(column=1, row=7, pady=5, sticky="ew", padx=5)
-
-DataCredito = Entry(frame, width=30, font="Poppins 10")
-DataCredito.grid(column=1, row=8, pady=5, sticky="ew", padx=5)
-
-VigenciaFinal = Entry(frame, width=30, font="Poppins 10")
-VigenciaFinal.grid(column=1, row=9, pady=5, sticky="ew", padx=5)
-
-botaocheck = Checkbutton(frame, text="Indicado?")
-botaocheck.grid(column=2, row=10, pady=5, sticky="ew", padx=5)
-
-Indicador = Entry(frame, width=30, font="Poppins 10")
-Indicador.grid(column=1, row=10, pady=5, sticky="ew", padx=5)
-
-# Botão para adicionar vendas
-botao_cadastrar = Button(frame, text="Cadastrar Venda", font="Poppins 10 bold", bg="#00796b", fg="white",
-                         relief="solid")
-botao_cadastrar.grid(column=0, row=12, columnspan=2, pady=20)
+with dpg.window(tag="App"):
+    xlsx.xlsx_state()
+    top_msg = dpg.add_text('Bem vindo ao cadastro de clientes',)
+    text_inputs()
 
 
 
+    btn = dpg.add_button(label="Salvar", callback=button_callback)
+    
+    #dpg.add_file_dialog(directory_selector=True, show=False, tag="file_dialog_id",width=700 ,height=400)
+    #dpg.add_button(label="Directory Selector", callback=lambda: dpg.show_item("file_dialog_id"))
 
+    dpg.add_button(label="Button 2")
+
+    #tabela para informar usuarios cadastrados
+    with dpg.table(header_row=False, row_background=True,
+                   borders_innerH=True, borders_outerH=True, borders_innerV=True,
+                   borders_outerV=True):
+
+       
+        dpg.add_table_column()
+
+        for i in range(0, 4):
+            with dpg.table_row():
+                for j in range(0, 3):
+                    dpg.add_text(f"Row{i} Column{j}")
+
+
+dpg.create_viewport(title='Minhas Vendas',small_icon='icons/icon.ico', width=800, height=600, resizable=False)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+#dpg.show_style_editor()
+#dpg.show_font_manager()
+dpg.set_primary_window("App", True)
+dpg.start_dearpygui()
+dpg.destroy_context()
